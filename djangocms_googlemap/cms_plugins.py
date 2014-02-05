d41ae968247d0c20bff312495d71c609859e3c6f
Kim Thoenen <kim@smuzey.ch>
2014-02-05 13:18:52 +0100

    moved out of the cms at divio/django-cms@108b07bcc756840a7ef62d254ce82ee9be564878

---
+++ djangocms_googlemap/cms_plugins.py
@@ -0,0 +1,36 @@
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from forms import GoogleMapForm
+from models import GoogleMap
+
+
+class GoogleMapPlugin(CMSPluginBase):
+    model = GoogleMap
+    name = _("Google Map")
+    render_template = "cms/plugins/googlemap.html"
+    admin_preview = False
+    form = GoogleMapForm
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'address', ('zipcode', 'city',),
+                       'content', 'zoom', ('lat', 'lng'),),
+        }),
+        (_('Advanced'), {
+            'fields': (('route_planer', 'route_planer_title'),
+                       ('width', 'height',), 'info_window', 'scrollwheel',
+                       'double_click_zoom', 'draggable', 'keyboard_shortcuts',
+                       'pan_control', 'zoom_control', 'street_view_control'),
+        }),
+    )
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'object': instance,
+            'placeholder': placeholder,
+        })
+        return context
+
+plugin_pool.register_plugin(GoogleMapPlugin)
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from djangocms_googlemap import __version__
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='djangocms-googlemap',
+    version=__version__,
+    description='Google Maps plugin for django CMS',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/divio/djangocms-googlemap',
+    packages=['djangocms_googlemap', 'djangocms_googlemap.migrations'],
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False
+)
