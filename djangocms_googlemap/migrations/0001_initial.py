692d35d6122e6b15d5f175b34ed31196929fa8b0
Kim Thoenen <kim@smuzey.ch>
2014-02-06 12:07:05 +0100

    Updated migrations to allow move from old cms

--- djangocms_googlemap/migrations/0001_initial.py
+++ djangocms_googlemap/migrations/0001_initial.py
@@ -2,38 +2,43 @@
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
+from django.db import models, connection
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'GoogleMap'
-        db.create_table(u'djangocms_googlemap_googlemap', (
-            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
-            ('address', self.gf('django.db.models.fields.CharField')(max_length=150)),
-            ('zipcode', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('city', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('content', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('zoom', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=13)),
-            ('lat', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=10, decimal_places=6, blank=True)),
-            ('lng', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=10, decimal_places=6, blank=True)),
-            ('route_planer_title', self.gf('django.db.models.fields.CharField')(default=u'Calculate your fastest way to here', max_length=150, null=True, blank=True)),
-            ('route_planer', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('width', self.gf('django.db.models.fields.CharField')(default='100%', max_length=6)),
-            ('height', self.gf('django.db.models.fields.CharField')(default='400px', max_length=6)),
-            ('info_window', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('scrollwheel', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('double_click_zoom', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('draggable', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('keyboard_shortcuts', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('pan_control', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('zoom_control', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('street_view_control', self.gf('django.db.models.fields.BooleanField')(default=True)),
-        ))
-        db.send_create_signal(u'djangocms_googlemap', ['GoogleMap'])
-
+        table_names = connection.introspection.table_names()
+        if 'cmsplugin_googlemap' in table_names:
+            db.rename_table('cmsplugin_googlemap', 'djangocms_googlemap_googlemap')
+        elif 'googlemap_googlemap' in table_names:
+            db.rename_table('googlemap_googlemap', 'djangocms_googlemap_googlemap')
+        else:
+            # Adding model 'GoogleMap'
+            db.create_table(u'djangocms_googlemap_googlemap', (
+                (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+                ('title', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+                ('address', self.gf('django.db.models.fields.CharField')(max_length=150)),
+                ('zipcode', self.gf('django.db.models.fields.CharField')(max_length=30)),
+                ('city', self.gf('django.db.models.fields.CharField')(max_length=100)),
+                ('content', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+                ('zoom', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=13)),
+                ('lat', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=10, decimal_places=6, blank=True)),
+                ('lng', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=10, decimal_places=6, blank=True)),
+                ('route_planer_title', self.gf('django.db.models.fields.CharField')(default=u'Calculate your fastest way to here', max_length=150, null=True, blank=True)),
+                ('route_planer', self.gf('django.db.models.fields.BooleanField')(default=False)),
+                ('width', self.gf('django.db.models.fields.CharField')(default='100%', max_length=6)),
+                ('height', self.gf('django.db.models.fields.CharField')(default='400px', max_length=6)),
+                ('info_window', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('scrollwheel', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('double_click_zoom', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('draggable', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('keyboard_shortcuts', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('pan_control', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('zoom_control', self.gf('django.db.models.fields.BooleanField')(default=True)),
+                ('street_view_control', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ))
+            db.send_create_signal(u'djangocms_googlemap', ['GoogleMap'])
 
     def backwards(self, orm):
         # Deleting model 'GoogleMap'
